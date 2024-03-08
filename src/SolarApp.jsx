85dea340aee019df19a47fb5e5230b41d4e88462
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-03-08 14:38:22 +0100

    new elements

--- src/SolarApp.jsx
+++ src/SolarApp.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
 import Presentation from './components/Presentation'
+import BodyContainer from './components/BodyContainer'
 
 function SolarApp() {
   return (
     <div className="SolarApp">
-        <Header/>
-        <Presentation/>
+      <Header />
+      <Presentation />
+      <BodyContainer/>
     </div>
   )
 }
---
+++ src/components/BodyContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/BodyContainer.css'
+
+const BodyContainer = () => {
+  return (
+    <article className="body-container">
+        <div className="calculator">
+
+        </div>
+        <div className="information">
+            
+        </div>
+    </article>
+  )
+}
+
+export default BodyContainer
