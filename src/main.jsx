0b20133a01d6d7266d7bd27450c4a8db682e1b2f
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-03-15 09:21:23 +0100

    from blowser router to hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import SolarApp from './SolarApp.jsx'
 import './index.css'
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <React.StrictMode>
-    <SolarApp />
-    <ToastContainer/>
-  </React.StrictMode>,
-  </BrowserRouter>
+  <HashRouter>
+    <React.StrictMode>
+      <SolarApp />
+      <ToastContainer />
+    </React.StrictMode>,
+  </HashRouter>
 )
